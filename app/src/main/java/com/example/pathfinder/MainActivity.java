db4093cae0c19a80d956fa30fb02fc89a45e1dec
Yinyang-01 <atthaleen@gmail.com>
2023-01-05 02:36:32 +0800

    Interface Fixed -navigation done -inconsistency on the UI fixed -on click listener removed -added app info on sign out
    for improvements:
    -Seekbar
    -Menu drawer

--- app/src/main/java/com/example/pathfinder/MainActivity.java
+++ app/src/main/java/com/example/pathfinder/MainActivity.java
@@ -53,6 +53,7 @@ public class MainActivity extends AppCompatActivity {
                         return true;
                     case R.id.sign_out:
                        getSupportFragmentManager().beginTransaction().replace(R.id.container,confirmsignoutFragment).commit();
+                        return true;
                 }
                 return false;
             }
